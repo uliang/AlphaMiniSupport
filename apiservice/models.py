0a8114be84dc9a1ff36bb62b8ff7701bda9a6a6a
Tang U-Liang <uliang6482@gmail.com>
2020-09-17 14:31:02 +0800

    updated database model and migrated

--- apiservice/models.py
+++ apiservice/models.py
@@ -1,3 +1,4 @@
+from datetime import date
 from django.db import models
 
 # Create your models here.
@@ -7,10 +8,12 @@ class Message(models.Model) :
     message = models.CharField(max_length=1000, verbose_name='Message')
     created_on = models.DateTimeField(verbose_name="Date created", auto_now_add=True)
     updated_on = models.DateTimeField(verbose_name="Date last updated", auto_now=True)
+    message_start_date = models.DateField(verbose_name="Day to start playing message", default=date.today) 
+    message_end_date = models.DateField(verbose_name="Day to end playing message", default=date.today)
     message_start = models.TimeField(verbose_name="Repeat message start time") 
     message_end = models.TimeField(verbose_name="Repeat message stop time", null=True, blank=True)
-    message_duration = models.DurationField(verbose_name="Repeat message duration", null=True, blank=True)
-    frequency = models.DurationField(verbose_name="Repeat message every")
+    message_duration = models.DurationField(verbose_name="Repeat message duration (hh:mm:ss)", null=True, blank=True)
+    frequency = models.DurationField(verbose_name="Repeat message every (hh:mm:ss)")
     active = models.BooleanField(verbose_name="Active message", default=True)
 
     def __str__(self) : 
